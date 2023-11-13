efff0ead2126fcd6e0e6935194e396d2487a18ac
Faurin <111374977+FaurinFox@users.noreply.github.com>
2023-11-13 17:50:23 +0200

    Create the initial express host

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tiny-node-rest
 Does a very specific thing i happen to need, that's it.
+
+This was specifically for node v v16.16.0, so not the latest by any means. Shouldn't matter much though, given this whole thing is not meant to be used by anyone else than self, and NVM exists should this be run years to the future
---
+++ simple-rest.mjs
@@ -0,0 +1,33 @@
+import express from 'express';
+import config from './conf.json' assert {type: 'json'};
+
+const app = express();
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+    res.send("Hello There! You're probably not supposed to see this, I think you need a specific endpoint.");
+});
+
+app.get('/sl-cda', (req, res) => {
+    if (req.query.secret) {
+        // Has secret associated, check it
+        if (req.query.secret == config.secret) {
+            res.send("You got it");
+            // Do things here later, possibly email
+        }else{
+            res.status(401).json({error: 'Unauthorized'});
+            // While this locally will return express's 401, on the domain
+            // Nginx will be configured to override and return its own.
+        }
+    }else{
+        res.status(401).json({error: 'Unauthorized, secret missing'});
+        // Much the same as the above comments.
+    }
+});
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+export default app;
