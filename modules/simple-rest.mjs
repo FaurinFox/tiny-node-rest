5e29d087486df6ce7cfddb489ac0d380188220a9
Faurin <111374977+FaurinFox@users.noreply.github.com>
2023-11-13 20:41:03 +0200

    Add a emailer, move to modules

--- modules/simple-rest.mjs
+++ modules/simple-rest.mjs
@@ -1,5 +1,6 @@
 import express from 'express';
-import config from './conf.json' assert {type: 'json'};
+import config from '../conf.json' assert {type: 'json'};
+import mail from './mailer.mjs';
 
 const app = express();
 
@@ -15,6 +16,17 @@ app.get('/sl-cda', (req, res) => {
         if (req.query.secret == config.secret) {
             res.send("You got it");
             // Do things here later, possibly email
+            try {
+                mail({
+                    from: 'noreply@fifox.fi',
+                    to: config.mailConfig.to,
+                    subject: 'This is a test email',
+                    text: 'And this is the body of the test email'
+                });
+            } catch (error) {
+                console.error(error);
+            }
+
         }else{
             res.status(406).json({error: 'Unauthorized, not accepted'});
             // While this locally will return express's 406, on the domain
